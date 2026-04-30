e142c3baef21c73e0fa9ebf3f3aff378a286d41e
AnshumanSharma147 <anshumansharma162008@gmail.com>
2026-04-30 19:35:24 +0530

    Will model be commited?

--- model.py
+++ model.py
@@ -1,2 +1,31 @@
-print("hi")
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+
+data = pd.read_csv("https://raw.githubusercontent.com/justmarkham/pycon-2016-tutorial/master/data/sms.tsv", sep="\t", names=["label", "message"])
+
+data['label'] = data['label'].map({'ham': 0, 'spam': 1})
+
+X_train, X_test, y_train, y_test = train_test_split(data['message'], data['label'], test_size=0.2)
+
+vectorizer = TfidfVectorizer()
+X_train_vec = vectorizer.fit_transform(X_train)
+X_test_vec = vectorizer.transform(X_test)
+
+model = MultinomialNB()
+model.fit(X_train_vec, y_train)
+
+y_pred = model.predict(X_test_vec)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+
+while True:
+    msg = input("Enter message: ")
+    msg_vec = vectorizer.transform([msg])
+    prediction = model.predict(msg_vec)
+
+    print("Spam" if prediction[0] == 1 else "Not Spam")
+    
 
